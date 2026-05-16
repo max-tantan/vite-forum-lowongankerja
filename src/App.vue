eb3fe7f8cab70037690af5827817ead3c0a95001
max-tantan <fatanjamterbang@gmail.com>
2026-05-16 10:44:58 +0700

    refactor: restructure layout to support responsive design with specific mobile and desktop views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
 import AndroidLayout from './layout/AndroidLayout.vue'
 </script>
 
 <template>
-  <AndroidLayout>
-    <RouterView />
-  </AndroidLayout>
+  <!-- Desktop Layout: visible on md and above -->
+  <div class="hidden md:flex min-h-screen flex-col bg-gray-50">
+    <Navbar />
+    <main class="container mx-auto flex-1 px-4 py-6">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+
+  <!-- Mobile / Android Layout: visible below md -->
+  <div class="md:hidden">
+    <AndroidLayout>
+      <RouterView />
+    </AndroidLayout>
+  </div>
 </template>
